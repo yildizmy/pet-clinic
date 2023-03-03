c3b75801e5d5d722d820c56060b95caa78247bd0
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-03 10:36:35 +0300

    feat: implement class to keep constant variables

---
+++ backend/src/main/java/com/github/yildizmy/common/Constants.java
@@ -0,0 +1,14 @@
+package com.github.yildizmy.common;
+
+/**
+ * Constant variables used in the project
+ */
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String TRACE = "trace";
+    public static final String DATE_FORMAT = "dd.MM.yyyy";
+    public static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm:ss";
+}
