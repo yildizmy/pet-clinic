b26a04f1b7f601fb980f2b727dadc85607ef58ed
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-03 10:58:03 +0300

    feat: implement controllers

---
+++ backend/src/main/java/com/github/yildizmy/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.github.yildizmy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+}
