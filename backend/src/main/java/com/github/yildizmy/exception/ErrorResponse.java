4ca8e21bd19330eba67e109b3330c7570a3e0fe2
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-03 10:40:51 +0300

    feat: implement global exception handling

---
+++ backend/src/main/java/com/github/yildizmy/exception/ErrorResponse.java
@@ -0,0 +1,38 @@
+package com.github.yildizmy.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Builds exception handler response in a proper format with status, message, stackTrace and errors
+ */
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+    private final int status;
+    private final String message;
+    private String stackTrace;
+    private List<ValidationError> errors;
+
+    // @Data
+    private record ValidationError(String field, String message) {
+    }
+
+    /**
+     * Adds validation error for handling unprocessable entity
+     *
+     * @param field
+     * @param message
+     */
+    public void addValidationError(String field, String message) {
+        if (Objects.isNull(errors)) {
+            errors = new ArrayList<>();
+        }
+        errors.add(new ValidationError(field, message));
+    }
+}
