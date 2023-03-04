e84aed9c1d0beb36d44d2cbcdc39855da14f3c9c
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-04 10:17:21 +0300

    feat: add new variables

--- backend/src/main/java/com/github/yildizmy/common/Constants.java
+++ backend/src/main/java/com/github/yildizmy/common/Constants.java
@@ -11,4 +11,25 @@ public final class Constants {
     public static final String TRACE = "trace";
     public static final String DATE_FORMAT = "dd.MM.yyyy";
     public static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm:ss";
+
+    public static final String VALIDATION_ERROR = "Validation error. Check 'errors' field for details";
+    public static final String METHOD_ARGUMENT_NOT_VALID = "MethodArgumentNotValid exception";
+    public static final String SUCCESS = "Success";
+    public static final String NOT_FOUND = "Requested element is not found";
+    public static final String NOT_FOUND_RECORD = "Not found any record";
+    public static final String NOT_FOUND_TYPE = "Requested type is not found";
+    public static final String NOT_FOUND_PET = "Requested pet is not found";
+    public static final String NOT_FOUND_USER = "Requested user is not found";
+    public static final String ALREADY_EXISTS = "Requested element is already exists";
+    public static final String ALREADY_EXISTS_TYPE = "Type with the same name is already exists";
+    public static final String ALREADY_EXISTS_USER = "User with the same username is already exists";
+    public static final String CREATED_TYPE = "Type is created";
+    public static final String CREATED_PET = "Pet is created";
+    public static final String CREATED_USER = "User is created";
+    public static final String UPDATED_TYPE = "Type is updated";
+    public static final String UPDATED_PET = "Pet is updated";
+    public static final String UPDATED_USER = "User is updated";
+    public static final String DELETED_TYPE = "Type is deleted";
+    public static final String DELETED_PET = "Owner is deleted";
+    public static final String DELETED_USER = "User is deleted";
 }
