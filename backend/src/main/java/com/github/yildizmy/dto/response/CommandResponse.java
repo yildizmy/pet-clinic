2f6c36a375ec15ea629fe23b55b87a1945d8306d
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-04 10:16:58 +0300

    refactor: remove unnecessary annotations

--- backend/src/main/java/com/github/yildizmy/dto/response/CommandResponse.java
+++ backend/src/main/java/com/github/yildizmy/dto/response/CommandResponse.java
@@ -1,14 +1,10 @@
 package com.github.yildizmy.dto.response;
 
 import lombok.Builder;
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
 
 /**
  * Data Transfer Object used for returning id value of the saved entity
  */
-@Value
-@RequiredArgsConstructor
 @Builder
 public record CommandResponse(Long id) {
 }
