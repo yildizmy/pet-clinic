712dcc9a302e1979ffdadc9220d5fc186e17f48d
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-05 13:30:04 +0300

    refactor: remove unused imports

--- backend/src/main/java/com/github/yildizmy/model/UserDetailsImpl.java
+++ backend/src/main/java/com/github/yildizmy/model/UserDetailsImpl.java
@@ -1,7 +1,6 @@
-package com.github.yildizmy.service;
+package com.github.yildizmy.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.github.yildizmy.model.User;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
