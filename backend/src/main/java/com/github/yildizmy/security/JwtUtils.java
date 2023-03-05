19942856c39c3a8aa54200ebb811fd3ff4df2879
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-05 11:40:53 +0300

    feat: implement util class for jwt

---
+++ backend/src/main/java/com/github/yildizmy/security/JwtUtils.java
@@ -0,0 +1,58 @@
+package com.github.yildizmy.security;
+
+import com.github.yildizmy.service.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+import static com.github.yildizmy.common.Constants.*;
+
+/**
+ * Utility class for Jwt related tasks
+ */
+@Component
+@Slf4j(topic = "JwtUtils")
+public class JwtUtils {
+
+    @Value("${app.security.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${app.security.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            log.error(INVALID_JWT_SIGN, e.getMessage());
+        } catch (MalformedJwtException e) {
+            log.error(INVALID_JWT_TOKEN, e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error(JWT_EXPIRED, e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error(JWT_UNSUPPORTED, e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error(JWT_EMPTY, e.getMessage());
+        }
+        return false;
+    }
+}
