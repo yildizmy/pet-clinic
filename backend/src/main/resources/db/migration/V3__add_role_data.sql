4374a75325a60cac12fb58886d3b7f887527925c
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-05 09:56:24 +0300

    feat: add new migration scripts for user and role tables

---
+++ backend/src/main/resources/db/migration/V3__add_role_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.role (id, "type") VALUES (1, 'ROLE_USER');
+INSERT INTO public.role (id, "type") VALUES (2, 'ROLE_ADMIN');
---
+++ backend/src/main/resources/db/migration/V4__add_user_role_data.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.user_role (user_id, role_id) VALUES(1, 2);
