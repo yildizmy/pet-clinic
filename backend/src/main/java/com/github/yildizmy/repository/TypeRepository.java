645647c41b50f53a3af1c2a05a385d13f0f05d08
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-05 11:41:47 +0300

    refactor: remove unused method

--- backend/src/main/java/com/github/yildizmy/repository/TypeRepository.java
+++ backend/src/main/java/com/github/yildizmy/repository/TypeRepository.java
@@ -7,7 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TypeRepository extends JpaRepository<Type, Long> {
 
-    boolean existsById(Long id);
-
     boolean existsByNameIgnoreCase(String name);
 }
