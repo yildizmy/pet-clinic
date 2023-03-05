b88fe3cb47da34e16f300b3ad6717d307609338d
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-05 13:28:19 +0300

    refactor: move class to another package

--- backend/src/main/java/com/github/yildizmy/security/JwtUtils.java
+++ backend/src/main/java/com/github/yildizmy/security/JwtUtils.java
@@ -1,6 +1,6 @@
 package com.github.yildizmy.security;
 
-import com.github.yildizmy.service.UserDetailsImpl;
+import com.github.yildizmy.model.UserDetailsImpl;
 import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
