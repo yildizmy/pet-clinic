5e5033ce92b0d8413df2babf5b7475d4c28b2a87
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-05 11:43:20 +0300

    refactor: add new method

--- backend/src/main/java/com/github/yildizmy/repository/UserRepository.java
+++ backend/src/main/java/com/github/yildizmy/repository/UserRepository.java
@@ -4,10 +4,12 @@ import com.github.yildizmy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsById(Long id);
-
     boolean existsByUsernameIgnoreCase(String name);
+
+    Optional<User> findByUsername(String username);
 }
