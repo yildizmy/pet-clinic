3075d357ec0fc8e336c32977b53da686611696aa
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-05 11:41:24 +0300

    refactor: update constant variables

--- backend/src/main/java/com/github/yildizmy/common/Constants.java
+++ backend/src/main/java/com/github/yildizmy/common/Constants.java
@@ -14,12 +14,21 @@ public final class Constants {
 
     public static final String VALIDATION_ERROR = "Validation error. Check 'errors' field for details";
     public static final String METHOD_ARGUMENT_NOT_VALID = "MethodArgumentNotValid exception";
+    public static final String DELETED_USER = "User is deleted";
+    public static final String CANNOT_SET_AUTH = "Cannot set user authentication: {}";
+    public static final String INVALID_JWT_SIGN = "Invalid JWT signature: {}";
+    public static final String INVALID_JWT_TOKEN = "Invalid JWT token: {}";
+    public static final String JWT_EXPIRED = "JWT token is expired: {}";
+    public static final String JWT_UNSUPPORTED = "JWT token is unsupported: {}";
+    public static final String JWT_EMPTY = "JWT claims string is empty: {}";
     public static final String SUCCESS = "Success";
     public static final String NOT_FOUND = "Requested element is not found";
     public static final String NOT_FOUND_RECORD = "Not found any record";
     public static final String NOT_FOUND_TYPE = "Requested type is not found";
     public static final String NOT_FOUND_PET = "Requested pet is not found";
     public static final String NOT_FOUND_USER = "Requested user is not found";
+    public static final String NOT_FOUND_USERNAME = "User Not Found with username: %s";
+    public static final String NOT_FOUND_ROLE = "Requested role is not found";
     public static final String ALREADY_EXISTS = "Requested element is already exists";
     public static final String ALREADY_EXISTS_TYPE = "Type with the same name is already exists";
     public static final String ALREADY_EXISTS_USER = "User with the same username is already exists";
@@ -31,5 +40,5 @@ public final class Constants {
     public static final String UPDATED_USER = "User is updated";
     public static final String DELETED_TYPE = "Type is deleted";
     public static final String DELETED_PET = "Owner is deleted";
-    public static final String DELETED_USER = "User is deleted";
+
 }
