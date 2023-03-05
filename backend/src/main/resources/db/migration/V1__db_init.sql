a4b8c9bf44b76369acc19b9a305311cc61256229
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-05 09:55:38 +0300

    refactor: update migration script

--- backend/src/main/resources/db/migration/V1__db_init.sql
+++ backend/src/main/resources/db/migration/V1__db_init.sql
@@ -1,5 +1,7 @@
 CREATE SEQUENCE IF NOT EXISTS sequence_pet START WITH 1 INCREMENT BY 5;
 
+CREATE SEQUENCE IF NOT EXISTS sequence_role START WITH 1 INCREMENT BY 5;
+
 CREATE SEQUENCE IF NOT EXISTS sequence_type START WITH 1 INCREMENT BY 5;
 
 CREATE SEQUENCE IF NOT EXISTS public.sequence_user START WITH 1 INCREMENT BY 5;
@@ -12,6 +14,13 @@ CREATE TABLE pet
     type_id    BIGINT      NOT NULL,
     user_id    BIGINT      NOT NULL,
     CONSTRAINT pk_pet PRIMARY KEY (id)
+);
+
+CREATE TABLE role
+(
+    id   BIGINT      NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    CONSTRAINT pk_role PRIMARY KEY (id)
 );
 
 CREATE TABLE type
@@ -27,10 +36,20 @@ CREATE TABLE public."user"
     id         BIGINT       NOT NULL,
     first_name VARCHAR(50)  NOT NULL,
     last_name  VARCHAR(50)  NOT NULL,
-    username   VARCHAR(50)  NOT NULL,
-    password   VARCHAR(120) NOT NULL,
+    username   VARCHAR(20)  NOT NULL,
+    password   VARCHAR(100) NOT NULL,
     CONSTRAINT pk_user PRIMARY KEY (id)
 );
+
+CREATE TABLE public.user_role
+(
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    CONSTRAINT pk_user_role PRIMARY KEY (role_id, user_id)
+);
+
+ALTER TABLE role
+    ADD CONSTRAINT uc_role_type UNIQUE (type);
 
 ALTER TABLE type
     ADD CONSTRAINT uc_type_name UNIQUE (name);
@@ -43,3 +62,9 @@ ALTER TABLE pet
 
 ALTER TABLE pet
     ADD CONSTRAINT FK_PET_ON_USER FOREIGN KEY (user_id) REFERENCES public."user" (id);
+
+ALTER TABLE public.user_role
+    ADD CONSTRAINT fk_user_role_on_role FOREIGN KEY (role_id) REFERENCES role (id);
+
+ALTER TABLE public.user_role
+    ADD CONSTRAINT fk_user_role_on_user FOREIGN KEY (user_id) REFERENCES public."user" (id);
