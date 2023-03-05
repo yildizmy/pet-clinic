9e003690852d43b2f1686501582d275605008acf
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-05 11:58:27 +0300

    refactor: update methods for role integration

--- backend/src/main/java/com/github/yildizmy/service/UserService.java
+++ backend/src/main/java/com/github/yildizmy/service/UserService.java
@@ -7,14 +7,22 @@ import com.github.yildizmy.dto.response.CommandResponse;
 import com.github.yildizmy.dto.response.UserResponse;
 import com.github.yildizmy.exception.ElementAlreadyExistsException;
 import com.github.yildizmy.exception.NoSuchElementFoundException;
+import com.github.yildizmy.model.Role;
+import com.github.yildizmy.model.RoleType;
 import com.github.yildizmy.model.User;
 import com.github.yildizmy.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.text.WordUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.github.yildizmy.common.Constants.*;
 
@@ -26,6 +34,7 @@ import static com.github.yildizmy.common.Constants.*;
 @RequiredArgsConstructor
 public class UserService {
 
+    private final PasswordEncoder encoder;
     private final UserRepository userRepository;
     private final UserRequestMapper userRequestMapper;
     private final UserResponseMapper userResponseMapper;
@@ -79,7 +88,17 @@ public class UserService {
         if (userRepository.existsByUsernameIgnoreCase(request.getUsername()))
             throw new ElementAlreadyExistsException(ALREADY_EXISTS_USER);
 
-        final User user = userRepository.save(userRequestMapper.toEntity(request));
+        // add default role (ROLE_USER) to the user
+        final Set<Role> roles = new HashSet<>(Arrays.asList(new Role(1L, RoleType.ROLE_USER)));
+
+        // add ROLE_ADMIN role if requested by admin
+        if (request.getRoles().contains(RoleType.ROLE_ADMIN.name()))
+            roles.add(new Role(2L, RoleType.ROLE_ADMIN));
+
+        final User user = userRequestMapper.toEntity(request);
+        user.setPassword(encoder.encode(request.getPassword()));
+        user.setRoles(roles);
+        userRepository.save(user);
         log.info(CREATED_USER);
         return CommandResponse.builder().id(user.getId()).build();
     }
@@ -93,9 +112,18 @@ public class UserService {
     public CommandResponse update(UserRequest request) {
         final User user = userRepository.findById(request.getId())
                 .orElseThrow(() -> new NoSuchElementFoundException(NOT_FOUND_USER));
-        // in case username field in the request is not null, set it with current value to prevent update
-        request.setUsername(user.getUsername());
-        userRepository.save(userRequestMapper.toEntity(request));
+
+        // update admin role of the user based on the request
+        if (request.getRoles().contains(RoleType.ROLE_ADMIN.name()))
+            user.addRole(new Role(2L, RoleType.ROLE_ADMIN));
+        else
+            user.removeRole(new Role(2L, RoleType.ROLE_ADMIN));
+
+        user.setFirstName(WordUtils.capitalizeFully(request.getFirstName()));
+        user.setLastName(WordUtils.capitalizeFully(request.getLastName()));
+        user.setPassword(request.getPassword());
+        user.setPassword(encoder.encode(request.getPassword()));
+        userRepository.save(user);
         log.info(UPDATED_USER);
         return CommandResponse.builder().id(user.getId()).build();
     }
