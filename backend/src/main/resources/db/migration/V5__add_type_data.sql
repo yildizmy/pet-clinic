5ceaad1686cf82f0fa0921fd4e9eacfbc811196d
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-07 23:34:58 +0300

    feat: add migration for types

---
+++ backend/src/main/resources/db/migration/V5__add_type_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."type" (id, "name", description) VALUES (1, 'Dog', null);
+INSERT INTO public."type" (id, "name", description) VALUES (2, 'Cat', null);
+INSERT INTO public."type" (id, "name", description) VALUES (3, 'Bird', null);
