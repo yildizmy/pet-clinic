7469a6713632228882b35094983a71527639d13a
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-10 23:38:32 +0300

    refactor: update migration script to add new users

--- backend/src/main/resources/db/migration/V2__add_user_data.sql
+++ backend/src/main/resources/db/migration/V2__add_user_data.sql
@@ -1,2 +1,6 @@
 INSERT INTO public.user (id, first_name, last_name, username, "password")
-VALUES (1, 'John', 'Doe', 'johndoe', 'j@hNd@E');
+VALUES (1, 'John', 'Doe', 'johndoe', 'johnd@e');
+INSERT INTO public.user (id, first_name, last_name, username, "password")
+VALUES (2, 'Linda', 'Calvin', 'lindacalvin', 'lindac@lvin');
+INSERT INTO public.user (id, first_name, last_name, username, "password")
+VALUES (3, 'Jeffrey', 'Taylor', 'jeffreytaylor', 'jeffreyt@ylor');
