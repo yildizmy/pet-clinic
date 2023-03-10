6b105cc23c0a0808457c24b11d348eb64a006129
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-10 23:39:27 +0300

    refactor: update migration script to add new pets

---
+++ backend/src/main/resources/db/migration/V6__add_pet_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (1, 'Lassie', 1, 1);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (2, 'Tweety', 3, 1);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (3, 'Odie', 1, 2);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (4, 'Garfield', 2, 2);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (5, 'Tom', 2, 3);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (6, 'Felix ', 2, 3);
