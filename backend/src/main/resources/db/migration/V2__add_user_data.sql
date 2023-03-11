492a766a903321033a7e15f31aa0426fb9d0f7f6
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-11 23:24:57 +0300

    refactor: update password fields in migration script

--- backend/src/main/resources/db/migration/V2__add_user_data.sql
+++ backend/src/main/resources/db/migration/V2__add_user_data.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.user (id, first_name, last_name, username, "password")
-VALUES (1, 'John', 'Doe', 'johndoe', 'johnd@e');
+VALUES (1, 'John', 'Doe', 'johndoe', '$2a$10$MMOkMuO8zVcXl8YH2GrZSOYf/9zeC/sznGHRVzAq0T8.tzet7QJWq');
 INSERT INTO public.user (id, first_name, last_name, username, "password")
-VALUES (2, 'Linda', 'Calvin', 'lindacalvin', 'lindac@lvin');
+VALUES (2, 'Linda', 'Calvin', 'lindacalvin', '$2a$10$I5hOscIFqw73AU2/my0H0.vHAjI/rxXGcI49PB/jl8krTcM7VqkCy');
 INSERT INTO public.user (id, first_name, last_name, username, "password")
-VALUES (3, 'Jeffrey', 'Taylor', 'jeffreytaylor', 'jeffreyt@ylor');
+VALUES (3, 'Jeffrey', 'Taylor', 'jeffreytaylor', '$2a$10$0grDMvQ7mSRLDAS6zuGOp.0ycwhgAzyE2FgLHzCV8KaXXP2TtGJ/W');
