496d992ef6c02d9f5a052ed1b96b944303b33ba1
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-12 11:32:38 +0300

    refactor: update import package

--- backend/src/main/java/com/github/yildizmy/security/JwtUtils.java
+++ backend/src/main/java/com/github/yildizmy/security/JwtUtils.java
@@ -1,6 +1,5 @@
 package com.github.yildizmy.security;
 
-import com.github.yildizmy.model.UserDetailsImpl;
 import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
