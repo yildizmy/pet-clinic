ad75fa7ad519a73a37f6d160fa7688de9ebfe8bc
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-13 22:58:44 +0300

    refactor: add annotation for role based auth

--- backend/src/main/java/com/github/yildizmy/config/SecurityConfig.java
+++ backend/src/main/java/com/github/yildizmy/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -31,7 +33,6 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
             "/api/v1/auth/**",
-            "/api/v1/test/**",
             // --- for Swagger UI v2 ------------
             "/v2/api-docs",
             "/swagger-resources",
