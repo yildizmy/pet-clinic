890ca4faa81aa4777cfd309c506b1d49db1a696f
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-15 19:07:54 +0300

    refactor: update id fields in migration file

--- backend/src/main/resources/db/migration/V6__add_pet_data.sql
+++ backend/src/main/resources/db/migration/V6__add_pet_data.sql
@@ -4,9 +4,9 @@ INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (3, 'Tweety', 3, 1)
 INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (4, 'Charlie', 1, 1);
 INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (5, 'Lucy', 2, 1);
 INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (6, 'Angel', 3, 1);
-INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (1, 'Odie', 1, 2);
-INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (2, 'Garfield', 2, 2);
-INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (3, 'Jack', 3, 2);
-INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (1, 'Tom', 2, 3);
-INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (2, 'Felix', 2, 3);
-INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (3, 'Spike', 3, 3);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (7, 'Odie', 1, 2);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (8, 'Garfield', 2, 2);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (9, 'Jack', 3, 2);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (10, 'Tom', 2, 3);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (11, 'Felix', 2, 3);
+INSERT INTO public.pet (id, "name", type_id, user_id) VALUES (12, 'Spike', 3, 3);
