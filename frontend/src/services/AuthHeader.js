716cbe03abba5bf51c168221484581ece0d24b3c
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-16 21:19:26 +0300

    feat: implement authheader in a separate service

---
+++ frontend/src/services/AuthHeader.js
@@ -0,0 +1,9 @@
+import AuthService from "./AuthService";
+
+export default function AuthHeader() {
+  const token = AuthService.getCurrentUser()?.token;
+
+  return token
+    ? { Accept: "application/json", Authorization: `Bearer ${token}` }
+    : {};
+}
