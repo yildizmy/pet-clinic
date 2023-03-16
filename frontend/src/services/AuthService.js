e309f539626c0d44608e5eed8718918703f676d5
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-16 21:22:41 +0300

    feat: implement service for auth related tasks

---
+++ frontend/src/services/AuthService.js
@@ -0,0 +1,34 @@
+import axios from "./axios";
+
+const login = (body) => {
+  const url = "/auth/login";
+  return axios.post(url, body).then((response) => {
+    localStorage.setItem("user", JSON.stringify(response.data.data));
+    return response.data;
+  });
+};
+
+const signup = (body) => {
+  const url = "/auth/signup";
+  return axios.post(url, body).then((response) => {
+    return response.data;
+  });
+};
+
+const logout = () => {
+  debugger;
+  localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+
+const AuthService = {
+  login,
+  signup,
+  logout,
+  getCurrentUser,
+};
+
+export default AuthService;
