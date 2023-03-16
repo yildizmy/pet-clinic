21cba5a2e27dc3c4846d7dddcdce3a89ecbd99c4
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-16 21:26:39 +0300

    refactor: get user from service instead of localStorage

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import AuthService from "./services/AuthService";
 
 const PrivateRoute = () => {
   const isAuthenticated =
-    localStorage.getItem("currentUser") !== null &&
-    localStorage.getItem("currentUser") !== "undefined";
+    AuthService.getCurrentUser() !== null &&
+    AuthService.getCurrentUser() !== undefined;
 
   // if authorized, return an outlet that will render child elements; if not, return element that will navigate to login page
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
