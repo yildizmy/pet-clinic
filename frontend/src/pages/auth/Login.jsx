74faa4a630e48dab4033b740279e021f4d8a8f57
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-17 11:59:31 +0300

    refactor: reimplement login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,12 +1,13 @@
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import { FormControl } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
+import Checkbox from "@mui/material/Checkbox";
 import CssBaseline from "@mui/material/CssBaseline";
+import FormControlLabel from "@mui/material/FormControlLabel";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
@@ -22,6 +23,7 @@ const Login = () => {
     password: "",
   };
 
+  const pageTitle = "Log in";
   const { enqueueSnackbar } = useSnackbar();
   const navigate = useNavigate();
   const theme = createTheme();
@@ -55,78 +57,98 @@ const Login = () => {
   };
 
   return (
-    <FormControl>
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${"login.png"})`,
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "top",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
-              marginTop: 8,
+              my: 8,
+              mx: 4,
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
-              justifyContent: "center",
             }}
           >
             <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
-              Log in
+              {pageTitle}
             </Typography>
             <Box
               component="form"
-              onSubmit={handleSubmit}
               noValidate
+              onSubmit={handleSubmit}
               sx={{ mt: 1 }}
             >
               <TextField
                 margin="normal"
                 required
                 fullWidth
-                autoFocus
-                id="username"
                 label="Username"
+                id="username"
                 name="username"
                 autoComplete="username"
                 value={formValues.username}
                 onChange={handleInputChange}
+                autoFocus
               />
               <TextField
                 margin="normal"
                 required
                 fullWidth
-                name="password"
                 label="Password"
+                name="password"
                 type="password"
                 id="password"
                 autoComplete="current-password"
                 value={formValues.password}
                 onChange={handleInputChange}
               />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
               <Button
+                type="submit"
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
-                type="submit"
                 disabled={
                   formValues.username === "" || formValues.password === ""
                 }
               >
                 Sign In
               </Button>
-              <Grid container>
+              <Grid container justifyContent="flex-end">
                 <Grid item>
                   <Link href="/signup" variant="body2">
-                    {"Don't have an account? Sign Up"}
+                    Don't have an account? Sign Up
                   </Link>
                 </Grid>
               </Grid>
             </Box>
           </Box>
-        </Container>
-      </ThemeProvider>
-    </FormControl>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
