b1dac62dc3de85939c311dec7efb4455e606eac8
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-22 21:35:06 +0300

    refactor: format code

--- backend/src/main/java/com/github/yildizmy/service/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/github/yildizmy/service/UserDetailsServiceImpl.java
@@ -1,15 +1,16 @@
 package com.github.yildizmy.service;
 
-import com.github.yildizmy.common.Constants;
 import com.github.yildizmy.model.User;
-import com.github.yildizmy.security.UserDetailsImpl;
 import com.github.yildizmy.repository.UserRepository;
+import com.github.yildizmy.security.UserDetailsImpl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import static com.github.yildizmy.common.Constants.NOT_FOUND_USERNAME;
 
 /**
  * Service used for UserDetails related operations
@@ -24,7 +25,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         final User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format(Constants.NOT_FOUND_USERNAME, username)));
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(NOT_FOUND_USERNAME, username)));
         return UserDetailsImpl.build(user);
     }
 }
