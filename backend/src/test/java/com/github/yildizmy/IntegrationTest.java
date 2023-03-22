d8ce683abde98f7398b269fcea81ce6d5a071f6f
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-22 16:36:47 +0300

    refactor: remove unnecessary annotation

--- backend/src/test/java/com/github/yildizmy/IntegrationTest.java
+++ backend/src/test/java/com/github/yildizmy/IntegrationTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -20,7 +19,6 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 @AutoConfigureMockMvc
 @Transactional
 @ActiveProfiles("test")
-@WithMockUser("spring") // the value passed to userDetailsService method in SecurityConfig
 abstract public class IntegrationTest {
 
     @Autowired
